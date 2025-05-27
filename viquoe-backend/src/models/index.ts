5016c9e1f2d037e788b02df627cd7eaf7bb2e42f
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-05-27 18:48:40 +0100

    start Backend afresh

--- viquoe-backend/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-// This file exports model classes that define the structure of the data and interact with the database.
-
-export * from './User';
-export * from './Post';
-export * from './Comment';
